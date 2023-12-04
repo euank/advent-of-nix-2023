9bad83c4a6cca56fc05a19249dfa77bcfdeeec42
Euan Kemp <euank@euank.com>
2023-12-04 21:35:27 +0900

    day04: part2

--- day04/default.nix
+++ day04/default.nix
@@ -29,4 +29,41 @@ let
         else
           (pow 2 (numWinning - 1));
     in foldl' builtins.add 0 (map scoreLine parsed);
-in { part1 = part1Answer lines; }
+
+  # part2
+  part2Answer = lines:
+    let
+      parsed = map parseLine lines;
+      lineWinning = pline:
+        let
+          numWinning = foldl'
+            (acc: el: acc + (if pline.winning ? "${toString el}" then 1 else 0))
+            0 pline.nums;
+        in numWinning;
+
+      # Score is the current score
+      # line is the current line
+      # lines is remaining lines
+      # counts is the number of cards on all future lines currently
+      doIt = score: line: lines: counts:
+        let
+          count = head counts;
+          lw = lineWinning line;
+          nw = lw * count;
+          incCounts = n: cs:
+            if n == 0 then
+              cs
+            else if (length cs) == 0 then
+              [ ]
+            else
+              [ ((head cs) + count) ] ++ (incCounts (n - 1) (tail cs));
+          counts' = incCounts lw (tail counts);
+        in if (length lines) == 0 then
+          (score + count)
+        else
+          doIt (score + count) (head lines) (tail lines) counts';
+    in doIt 0 (head parsed) (tail parsed) (genList (x: 1) ((length lines) + 1));
+in {
+  part1 = part1Answer lines;
+  part2 = part2Answer lines;
+}
--- lib.nix
+++ lib.nix
@@ -5,6 +5,8 @@ let
     # x^n
     pow = x: n: if n == 1 then x else x * (pow x (n - 1));
 
-    splitStringWhitespace = s: pkgs.lib.flatten (builtins.filter builtins.isList (builtins.split "([^ ]+)" s));
+    splitStringWhitespace = s:
+      pkgs.lib.flatten
+      (builtins.filter builtins.isList (builtins.split "([^ ]+)" s));
   };
 in lib
