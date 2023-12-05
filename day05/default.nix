73d8a038f3fd71d2628084045d040c9ce1769fdf
Euan Kemp <euank@euank.com>
2023-12-05 23:16:35 +0900

    day05: part2

--- day05/default.nix
+++ day05/default.nix
@@ -28,6 +28,7 @@ let
       mappings = map parseMapping mappings;
     };
 
+  applySingleMapping = num: m: if m == null then num else num - m.start + m.to;
   doMapping = num: mapping:
     # See if this is in the mapping
     let
@@ -38,7 +39,7 @@ let
           1
         else
           0);
-    in if m == null then num else num - m.start + m.to;
+    in applySingleMapping m;
 
   part1Answer = input:
     let
@@ -51,4 +52,64 @@ let
       res = map (seed: doMappings seed parsed.mappings) parsed.seeds;
     in foldl' trivial.min (head res) (tail res);
 
-in { part1 = part1Answer input; }
+  # part2
+  # Map one range into possibly multiple ranges using the given mapping.
+  doRangeMappings = range: mapping:
+    let
+      start = head range;
+      end = elemAt range 1;
+      range' = tail (tail range);
+      # Find the mapping nearest to start
+      m =
+        findFirst (el: el.start >= start || ((el.start + el.len - 1) >= start))
+        null mapping;
+      mappingStart = trivial.max start m.start;
+      mappingEnd = trivial.min end (m.start + m.len - 1);
+    in if (length range) == 0 then
+    # We processed all ranges for this mapping
+      [ ]
+    else if m == null then
+    # null here means we have no mappings, numbers just go directly over
+      [ start end ] ++ (doRangeMappings range' mapping)
+    else if mappingStart > end || mappingEnd < start then
+    # The nearest mapping doesn't actually contain us; we map over directly
+      [ start end ] ++ (doRangeMappings range' mapping)
+    else
+    # Otherwise, we need to map the given range, and possibly carve out up to
+    # two new ranges (the range before the mapping and after)
+      let
+        # Map what we should
+        mappedRange = [
+          (applySingleMapping mappingStart m)
+          (applySingleMapping mappingEnd m)
+        ];
+        # Anything before the mapping
+        preMapped =
+          if start < mappingStart then [ start (mappingStart - 1) ] else [ ];
+        # Anything after
+        postMapped = if end > mappingEnd then [ (mappingEnd + 1) end ] else [ ];
+      in mappedRange
+      ++ (doRangeMappings (preMapped ++ postMapped ++ range') mapping);
+
+  part2Answer = input:
+    let
+      parsed = parseInput input;
+      # Ranges of (n, len) seem harder to reason about than (start, end) for me, map over to that.
+      normalizedSeeds = imap0 (i: el:
+        if (mod i 2) == 0 then el else (elemAt parsed.seeds (i - 1)) + el - 1)
+        parsed.seeds;
+      doMappings = ranges: mappings:
+        if (length mappings) == 0 then
+          ranges
+        else
+          let
+            h = head mappings;
+            nextRanges = doRangeMappings ranges h;
+          in doMappings nextRanges (tail mappings);
+      outRanges = doMappings normalizedSeeds parsed.mappings;
+    in foldl' trivial.min (head outRanges) (tail outRanges);
+
+in {
+  part1 = part1Answer input;
+  part2 = part2Answer input;
+}
