75cbed25fbcd27b005fc4444d2bc0d6975aa431f
Euan Kemp <euank@euank.com>
2023-12-06 23:07:36 +0900

    day06: part2

--- day06/default.nix
+++ day06/default.nix
@@ -20,13 +20,36 @@ let
       isWinning = time: maxTime: target: (maxTime - time) * time > target;
       waysToWinRace = race:
         let
-          # We could binary search, but these numbers are small, we're fnie
-          firstWin = findFirst (el: isWinning el race.time race.dist) null
-            (genList trivial.id race.time);
-          lastWin = foldr (el: lst:
-            if lst == null && isWinning el race.time race.dist then el else lst)
-            null (genList trivial.id race.time);
+          searchWin = times:
+            if length times == 0 then
+              null
+            else
+              let
+                pivot = elemAt times ((length times) / 2);
+                left = searchWin (sublist 0 ((length times) / 2) times);
+                right = searchWin
+                  (sublist (((length times) / 2) + 1) (length times) times);
+              in if isWinning pivot race.time race.dist then
+                if left != null then left else pivot
+              else
+                right;
+
+          firstWin = searchWin (genList trivial.id race.time);
+          lastWin = searchWin (reverseList (genList trivial.id race.time));
         in (lastWin - firstWin + 1);
     in foldl' (acc: race: acc * (waysToWinRace race)) 1 races;
 
-in { part1 = part1Answer (parseInput input); }
+  # part2
+  parseInput2 = input:
+    let
+      lines = splitString "\n" input;
+      time = toInt (concatStrings (tail (splitStringWhitespace (head lines))));
+      dist = toInt
+        (concatStrings (tail (splitStringWhitespace (head (tail lines)))));
+    in { inherit time dist; };
+  part2Answer = input: part1Answer [ (parseInput2 input) ];
+
+in {
+  part1 = part1Answer (parseInput input);
+  part2 = part2Answer input;
+}
