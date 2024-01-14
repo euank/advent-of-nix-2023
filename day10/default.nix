62d770826f686c235598a76c2bd8f966d1a92f62
Euan Kemp <euank@euank.com>
2024-01-14 19:12:42 +0900

    day10: part2
    I did google for this, but it works :D

--- day10/default.nix
+++ day10/default.nix
@@ -81,4 +81,54 @@ let
         else
           traverse (len + 1) (arr2.set seen cur.x cur.y true) grid next;
     in (traverse 1 (arr2.map (_: false) grid) grid s) / 2;
-in { part1 = part1Answer (parseInput input); }
+
+  # part2
+  # I googled around for calculating polygon area given that we know each
+  # vertex, and eventually found
+  # https://en.wikipedia.org/wiki/Shoelace_formula
+
+  vertsAndPathLen = grid:
+    let
+      s = findFirst (a: a.val == "S") null (flatten grid);
+      traverse = vxes: seen: grid: cur:
+        let
+          adj = findAdjacent grid cur;
+          adj1 = elemAt adj 0;
+          adj2 = elemAt adj 1;
+          next = if !(arr2.get seen adj1.x adj1.y) then
+            adj1
+          else if !(arr2.get seen adj2.x adj2.y) then
+            adj2
+          else
+            null;
+        in if next == null then
+          (vxes ++ [ cur ])
+        else
+          traverse (vxes ++ [ cur ]) (arr2.set seen cur.x cur.y true) grid next;
+    in rec {
+      verts = (traverse [ ] (arr2.map (_: false) grid) grid s);
+      len = length verts;
+    };
+
+  part2Answer = grid:
+    let
+      # all points on loop
+      vertsAndPath = vertsAndPathLen grid;
+      pathLen = vertsAndPath.len;
+      verts = vertsAndPath.verts;
+      # And now shoelace it
+      det = i:
+        let
+          cur = elemAt verts i;
+          next = elemAt verts (trivial.mod (i + 1) (length verts));
+        in cur.x * next.y - next.x * cur.y;
+      fullArea = (abs (foldl' (acc: el: acc + (det el)) 0
+        (genList trivial.id (length verts)))) / 2;
+      # Knock off part of the path, see https://en.wikipedia.org/wiki/Pick%27s_theorem
+      # Had to google a lot for that too, the +1 wasn't obvious to me.
+    in abs ((fullArea) - (pathLen / 2) + 1);
+
+in {
+  part1 = part1Answer (parseInput input);
+  part2 = part2Answer (parseInput input);
+}
